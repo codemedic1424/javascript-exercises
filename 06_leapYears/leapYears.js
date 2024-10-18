52c646e358808d12aa0196fc8767b287b4def853
codemedic1424 <sunnier.stopper.0x@icloud.com>
2024-10-17 20:10:15 -0400

    Assignments Complete 1-7. Commit for assignment update.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(x) {
+    if(x % 4 === 0 && (x % 400 === 0 ||  x % 100 !== 0)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
